43d4bf6c1a9016256635ba0f5c1b34e0d9fb5f0f
venkatsgithub1 <venkatsgithub@gmail.com>
2017-01-08 00:48:52 +0530

    OccupationsPivot.sql

---
+++ OccupationsPivot.sql
@@ -0,0 +1,1 @@
+select Doctor,Professor,Singer,Actor from (select name,occupation,row_number() over (partition by occupation order by name) as rnbr from occupations) pivot ( min( name) for (occupation) in ('Doctor' as Doctor,'Professor' as Professor,'Singer' as Singer,'Actor' as Actor)) order by rnbr ;
