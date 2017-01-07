dfeaa444de21a57706f38ad1cdcfb3aabfed8da7
venkatsgithub1 <venkatsgithub@gmail.com>
2017-01-08 00:52:33 +0530

    Update OccupationsPivot.sql

--- OccupationsPivot.sql
+++ OccupationsPivot.sql
@@ -1,6 +1,6 @@
-select Doctor,Professor,Singer,Actor 
-from 
-(select name,occupation,row_number() over (partition by occupation order by name) as rnbr from occupations) 
-pivot 
-( min( name) for (occupation) in ('Doctor' as Doctor,'Professor' as Professor,'Singer' as Singer,'Actor' as Actor)) 
-order by rnbr ;
+select Doctor,Professor,Singer,Actor from (
+SELECT NAME,OCCUPATION,ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME) AS RNBR FROM OCCUPATIONS )
+PIVOT
+(MIN (NAME) FOR OCCUPATION IN ('Doctor' as Doctor,'Professor' as Professor,'Singer' as Singer,'Actor' as Actor))
+order by RNBR
+;
